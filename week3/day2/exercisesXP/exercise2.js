363f83732e15b96af67d334b584cb3f03ea69479
ayoubidbarka <ayoub3idbarka@gmail.com>
2025-05-20 16:56:14 +0100

    week3

---
+++ week3/day2/exercisesXP/exercise2.js
@@ -0,0 +1,19 @@
+//execise2
+
+//my favorite colors
+//1
+let colors = ["green" , "yellow" , "blue" , "Gray" , "red"]
+console.log(colors)
+//2
+for (let i = 0 ; i < colors.length ; i++ ){
+    console.log(i+1 ,"choice is ", colors[i]);
+}
+//3
+let array_of_siffixes = ["st","nd","rd"]
+for(let i = 0 ; i < colors.length ; i++){
+    let num = i + 1 ;
+    let sufix = num <= 3  ? array_of_siffixes[i] : "th";
+    console.log(`My ${num}${sufix} choice is ${colors[i]}`)
+}
+
+
