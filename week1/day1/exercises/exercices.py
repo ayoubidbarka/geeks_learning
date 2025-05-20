c74fcc30ace1bb2bdc6afdab6730d83113a9df32
ayoubidbarka <ayoub3idbarka@gmail.com>
2025-05-20 10:46:59 +0100

    day

--- week1/day1/exercises/exercices.py
+++ week1/day1/exercises/exercices.py
@@ -28,6 +28,37 @@ friend_fav_numbers = {13, 3, 21}
 our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
 print(our_fav_numbers)
 #exercise6
+my_tuple = (1, 2, 3)
+#exercise7
+
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+
+basket.remove("Banana")
+basket.remove("Blueberries")
+basket.append("Kiwi")
+basket.insert(0, "Apples")
+apple_count = basket.count("Apples")
+print(f"Number of apples: {apple_count}")
+basket.clear()
+print(basket)  
+#exercise8
+
+sandwich_orders = [
+    "Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", 
+    "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", 
+    "Pastrami sandwich"
+]
+
+print("The deli has run out of pastrami.")
+while "Pastrami sandwich" in sandwich_orders:
+    sandwich_orders.remove("Pastrami sandwich")
+
+finished_sandwiches = []
+
+while sandwich_orders:
+    sandwich = sandwich_orders.pop(0)
+    print(f"I made your {sandwich.lower()}")
+    finished_sandwiches.append(sandwich)
 
 
 
@@ -38,15 +69,3 @@ print(our_fav_numbers)
 
 
 
-
-
-
-
-number = int(input("enter number: " ))
-length = int(input("enter length: "))
-liste = []
-result = 0
-for i in range(1,length+1) :
-    result += number * i 
-    result.append(liste)
-    print(liste)
