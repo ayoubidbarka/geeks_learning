75f822c9c97f287fd0fdd8c8c8cb18c3cfd27d7f
ayoubidbarka <ayoub3idbarka@gmail.com>
2025-05-22 15:50:26 +0100

    exerciseXP

---
+++ week3/day2/exercisesXP/exercise7.js
@@ -0,0 +1,8 @@
+//1
+const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+let friend = names 
+.map(name => name[0])//bach t7aded 1 alphabet mn kola name
+.sort() // bach tkharajhom mn []
+.join("")// bach tjma3hom ...
+//2
+console.log(friend)
