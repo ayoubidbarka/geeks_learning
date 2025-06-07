b1384abe4428f0385ac837abcc539f0ee747396f
ayoubidbarka <ayoub3idbarka@gmail.com>
2025-06-07 18:25:56 +0100

    week2

--- week2/day3/dailychallenge/challenge.sql
+++ week2/day3/dailychallenge/challenge.sql
@@ -1,3 +1,4 @@
+--part1
 CREATE TABLE Customer (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
@@ -8,9 +9,94 @@ CREATE TABLE CustomerProfile (
     id SERIAL PRIMARY KEY,
     isLoggedIn BOOLEAN DEFAULT false,
     customer_id INTEGER UNIQUE REFERENCES Customer(id)
+    Insert those customers
+    CONSTRAINT fk_customer
+        FOREIGN KEY (customer_id)
+        REFERENCES Customer(id)
+        ON DELETE CASCADE
 );
 INSERT INTO Customer (first_name, last_name)
 VALUES 
 ('John', 'Doe'),
 ('Jerome', 'Lalu'),
 ('Lea', 'Rive');
+
+INSERT INTO CustomerProfile (isLoggedIn , customer_id)
+values (
+    true,
+    (select id from Customer where first_name= 'John' and last_name='Doe');
+);
+INSERT INTO CustomerProfile (isLoggedIn , customer_id)
+values (
+    false;
+    (select id from customer where first_name= 'Jerome' and last_name='Lalu');
+);
+SELECT c.first_name
+FROM Customer c
+INNER JOIN CustomerProfile cp ON c.id = cp.customer_id
+WHERE cp.isLoggedIn = true;
+
+SELECT c.first_name, cp.isLoggedIn
+FROM Customer c
+LEFT JOIN CustomerProfile cp ON c.id = cp.customer_id;
+
+SELECT COUNT(*) AS not_logged_in_count
+FROM Customer c
+LEFT JOIN CustomerProfile cp ON c.id = cp.customer_id
+WHERE cp.isLoggedIn = false OR cp.isLoggedIn IS NULL;
+
+--part2
+
+create table  Book (
+  book_id SERIAL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  author VARCHAR(100) NOT NULL
+);
+insert into Book(title , author);
+('Alice In Wonderland', 'Lewis Carroll'
+'Harry Potter', 'J.K Rowling'
+'To kill a mockingbird', 'Harper Lee')
+
+CREATE TABLE Student (
+    student_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    age INT CHECK (age <= 15)
+);
+INSERT INTO Student (name, age) VALUES
+('John', 12),
+('Lera', 11),
+('Patrick', 10),
+('Bob', 14);
+CREATE TABLE Library (
+    book_fk_id INT,
+    student_fk_id INT,
+    borrowed_date DATE,
+    PRIMARY KEY (book_fk_id, student_fk_id),
+    CONSTRAINT fk_book FOREIGN KEY (book_fk_id) REFERENCES Books(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT fk_student FOREIGN KEY (student_fk_id) REFERENCES Student(student_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+INSERT INTO Library (book_fk_id, student_fk_id, borrowed_date) VALUES
+(
+    (SELECT id FROM Books WHERE title = 'Alice In Wonderland'),
+    (SELECT student_id FROM Student WHERE name = 'John'),
+    '2022-02-15'
+),
+(
+    (SELECT id FROM Books WHERE title = 'To kill a mockingbird'),
+    (SELECT student_id FROM Student WHERE name = 'Bob'),
+    '2021-03-03'
+),
+(
+    (SELECT id FROM Books WHERE title = 'Alice In Wonderland'),
+    (SELECT student_id FROM Student WHERE name = 'Lera'),
+    '2021-05-23'
+),
+(
+    (SELECT id FROM Books WHERE title = 'Harry Potter'),
+    (SELECT student_id FROM Student WHERE name = 'Bob'),
+    '2021-08-12'
+);
